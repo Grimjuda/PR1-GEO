7ec317b55494c623112ec38b9bd95679b8c47fd2
Grimjuda <hugoberrones@hotmail.com>
2021-05-03 18:28:40 -0500

    Practicas Firebase

---
+++ practicas firebase/basededatos.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDLtHaG79yjKvHuG56-p5j7QhU2zSQydho",
+    authDomain: "practicafirebase-geo.firebaseapp.com",
+    projectId: "practicafirebase-geo",
+    storageBucket: "practicafirebase-geo.appspot.com",
+    messagingSenderId: "80583287153",
+    appId: "1:80583287153:web:eb8e4e20edb523a18b56d0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
