877d61a9e232b3c12cb25bc776ecb4e97834a5d1
Grimjuda <hugoberrones@hotmail.com>
2021-06-17 18:37:42 -0500

    Proyecto geo

---
+++ proyecto/autorizacion.js
@@ -0,0 +1,39 @@
+const formaingresar = document.getElementById('formaingresar');
+formaingresar.addEventListener('submit',(e)=>{
+e.preventDefault();
+let correo = formaingresar['correo'].value;
+let contrasena = formaingresar['contrasena'].value;
+auth.signInWithEmailAndPassword(correo,contrasena).then(cred =>{
+    console.log(cred)
+    window.location = "proyecto.html";
+
+    formaingresar.reset();
+    formaingresar.querySelector('.error').innerHTML='';
+
+}).catch( err => {formaingresar.querySelector('.error').innerHTML=mensajeError(err.code); console.log(err)});
+});
+function mensajeError(codigo){
+let mensaje = ''
+switch (codigo){
+    case 'auth/wrong-password':
+    mensaje = 'Su contraseña no es correcta'
+    break
+    case 'auth/user-not-found':
+    mensaje = 'Usuario no encontrado'
+    break
+    case 'auth/weak-password':
+    mensaje = 'Contraseña debil'
+    break
+    default:
+        mensaje = 'Ocurrió un error al ingresar con este usuario'
+    
+}
+return mensaje;
+}
+const salir = document.getElementById('salir');
+salir.addEventListener('click',(e)=>{
+    e.preventDefault();
+    auth.signOut().then(() =>{
+        alert('El usuario ha salido del sistema')
+    })
+});
